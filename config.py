c37e7c0687aea3b39d531f7bd8d8a15f9f646ec5
temporisfiliaveritas1-boop <temporisfiliaveritas1@gmail.com>
2026-07-31 23:36:00 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
-# config.py
-
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 GROUP_ID  = int(os.environ.get("GROUP_ID"))
-ADMIN_ID  = int(os.environ.get("ADMIN_ID"))             # твой личный Telegram ID
+ADMIN_ID  = int(os.environ.get("ADMIN_ID"))
+
+ADMIN_IDS = [
+    int(x.strip())
+    for x in os.environ.get("ADMIN_IDS", "").split(",")
+    if x.strip()
+]
